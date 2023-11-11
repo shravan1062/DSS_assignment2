a4c4bd2649add517194c71c92d70fdfa3b3ba67d
shravan1062 <150520935+shravan1062@users.noreply.github.com>
2023-11-11 23:04:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz_game',
+    name='math_quiz',
     version='3.11.6',
     packages=find_packages(),
     install_requires=[
-        # Specify any dependencies here
-        'random',
     ],
-    entry_points={
-        'console_scripts': [
-            'math-quiz-game = math_quiz_game.quiz:math_quiz',
-        ],
-    },
 )
